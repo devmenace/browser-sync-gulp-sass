5ece0376ee3e587ca9f2f96034594e55257a86f9
Den21 <menacedev@gmail.com>
2020-07-04 19:29:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Usage:
+-----
+Clone repository.  
+
+Install dependencies `npm install`  
+
+Run `gulp` to start environment   
+
+All front end files will be located in `/public_html` folder
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// Serving static content from
+const source = 'public_html';
+const dest = source,
+    css = {
+        in: source + '/scss/style.scss',
+        watch: [source + '/scss/**/*'],
+        out: dest,
+        sassOpts: {
+            outputStyle: 'nested',
+            imagePath: '../images',
+            precision: 3,
+            errLogToConsole: true
+        }
+    };
+
+//  Sass compiler
+const sassFunc =
+    function sassFunc() {
+        return gulp.src(css.in)
+            .pipe(sass(css.sassOpts))
+            .pipe(gulp.dest(css.out));
+    };
+
+// Browser sync proxy server
+const bspFunc =
+    function bspFunc() {
+        browserSync.init({
+            port: 3020,
+            server: {
+                baseDir: source + '/'
+            },
+            ui: {
+                port: 4040
+            }
+        });
+        gulp.watch(source + '*/**/*').on('change', browserSync.reload);
+        gulp.watch(source + '**/*.html').on('change', browserSync.reload);
+        gulp.watch(source + 'scss/*.scss', sassFunc).on('change', browserSync.reload);
+        // gulp.watch(source + 'js/*.js').on('change', browserSync.reload);
+    };
+
+
+gulp.task('default', gulp.parallel(
+    gulp.series(bspFunc),
+));
+
+console.log(source + '/**/*.html') //?
+
+exports.sassFunc = sassFunc;
+exports.bspFunc = bspFunc;
+exports.default = 'default';
